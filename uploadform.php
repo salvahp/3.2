727e2e2271b178fd020587e6cd04273084d49354
Dan Mortensen <enklopida@gmail.com>
2016-05-16 23:30:27 +0200

    rettelser og noter til næste drift

--- uploadform.php
+++ uploadform.php
@@ -10,11 +10,30 @@
 	<input type="file" name="fileToUpload" id="fileToUpload">
 	<input type="submit" value="Upload Image" name="submit">
 </form>
-
-<!-- connect to database-->
 <?php
+//frbind til database
 	mysql_connect("localhost", "root", "") or die(mysql_error());
 	mysql_select_db("muratmoviedb") or die(mysql_error());
+
+//fil oplysninger 
+$file = $_FILES['fileToUpload']['tmp_name'];
+
+if (!isset($file))
+	echo "Please select an image.";
+	else{
+		$image = addslashes(file_get_content($_FILES['fileToUpload']['tmp_name']));
+		$image_name = addslashes($_FILES['image']['name']);
+		$image_size = getimagesize($_FILES['image']['tmp_name']);
+		
+		if ($image_size==FALSE)
+			echo "Det var ikke et billede.";
+		else {
+			$insert = mysql_query('INSERT INTO 
+			https://www.youtube.com/watch?v=pMLc4IXmYfg 
+Det er her jeg er nået til ');
+// Fortsæt i youtube videoen
+		}
+		}
 ?>
 </body>
 </html>
