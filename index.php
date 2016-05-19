c111776c88be9a9878d6b72c5735edffb1cdbc43
amandavejle <vejle.amanda@gmail.com>
2016-05-19 16:29:36 +0200

    meta keyword + description

--- index.php
+++ index.php
@@ -15,8 +15,8 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		<!-- for-mobile-apps -->
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-		<meta name="keywords" content="Champion Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
-Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+		<meta name="keywords" content="Kampagneside, Campus Bindslevs Plads, Silkeborg, Kultur, Aarhus17, " />
+        <meta name="description" content="Kampagneside for Campus Bindslevs Plads, hold dig opdateret på projektet her!">
 		<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false);
 		function hideURLbar(){ window.scrollTo(0,1); } </script>
 		<!-- //for-mobile-apps -->
